bec99a39c784a5a8abec146ef9c791591ebb84d2
Ting <baiyuting652@gmail.com>
2025-02-23 11:43:22 +1100

    add new slideshow

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Container from './Container'
 import MenuAnimation from './animata/list/menu-animation';
+import Expandable from './animata/carousel/expandable';
 
 const Hero = () => {
   return (
     <section id="Home" className='overflow-hidden py-20 sm:py-32 lg:pd-32 xl:pb-3'>
         <Container>
-            <div>
+            <div className="flex justify-between items-center">
                 {/* Left side: 3 main buttons: RENTAL GUIDE, FIND A HOME, APPLY DOCUMENTS */}
                 <MenuAnimation 
                     menuItems={[
@@ -16,6 +17,7 @@ const Hero = () => {
                     ]}
                 />
                 {/* Right side: Automatic Slideshow, click to see details */}
+                <Expandable className='fixed right-10 w-1/2 min-w-72 storybook-fix'/>
             </div>
         </Container>
     </section>
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -18,7 +18,24 @@ export default {
           blue: '#2A4C65',
           gray: '#EBEBE9'
         }
-      }
+      },
+      transitionTimingFunction: {
+        "minor-spring": "cubic-bezier(0.18,0.89,0.82,1.04)",
+      },
+      keyframes:{
+        "reveal-up": {
+        "0%": { opacity: "0", transform: "translateY(80%)" },
+        "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        "reveal-down": {
+          "0%": { opacity: "0", transform: "translateY(-80%)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        "content-blur": {
+          "0%": { filter: "blur(0.3rem)" },
+          "100%": { filter: "blur(0)" },
+        },
+      },
     }
   },
   plugins: [require('tailwindcss-animate')],
