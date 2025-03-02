b0aac08f332ef5b11f718a78047786bf97f7a161
Zach <zhangyibin481516@gmail.com>
2025-03-02 11:27:50 +1100

    feat: complete uthenticateToken

--- packages/backend/src/middlewares/auth.middleware.ts
+++ packages/backend/src/middlewares/auth.middleware.ts
@@ -1,10 +1,34 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-
 dotenv.config();
+const secretKey = process.env.JWT_SECRET || 'your_secret_key';
+import HttpError from '@/error/HttpError';
+import { Request, Response, NextFunction } from 'express';
+
+declare module 'express' {
+  interface Request {
+    user: any;
+  }
+}
 
 export function generateToken(userId: string): string {
-    const secretKey = process.env.JWT_SECRET || 'your_secret_key';
     const expiresIn = '1h';
     return jwt.sign({ userId }, secretKey, { expiresIn });
 }
+
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+  
+    if (token == null) {
+      return next(new HttpError(401, 'Token is missing'));
+    }
+  
+    jwt.verify(token, secretKey, (err: any, user: any) => {
+      if (err) {
+        return next(new HttpError(403, 'Token is not valid'));
+        }
+        req.user = user;
+      next();
+    });
+  };
