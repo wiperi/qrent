a602d924767947e2cd2d339070a127ff64a8d948
wiperi <solidtreepassing@gmail.com>
2025-03-30 15:10:01 +1100

    feat(api): get property list based on preference

--- packages/shared/utils/db.ts
+++ packages/shared/utils/db.ts
@@ -3,7 +3,7 @@ import { prisma } from '../prisma/client';
 async function clearDb() {
   await prisma.user.deleteMany();
   await prisma.userPreference.deleteMany();
-  await prisma.property.deleteMany();
+  // await prisma.property.deleteMany();
 }
 
 export { clearDb };
