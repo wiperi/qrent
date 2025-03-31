16b962aca62e1eae25723e300a7604f95e2d58a8
Ting Bai <baiyuting652@gmail.com>
2025-03-31 19:07:32 +1100

    fix: fix url writing (#25)
    * feat: add logo and login in landing router
    
    * style: update font for logo and login button
    
    * feat: add button component and update login NavLink to button
    
    * style: update color theme to Morandi
    
    * feat: add menu list animation
    
    * style: adjust spacing between list items for better usability
    
    * add new slideshow
    
    * feat: add language button
    
    * fix: change slideshow to fixed position on page
    
    * fix: correct typo
    
    * fix: resolve excessive height space issue
    
    * feat: add footer
    
    * feat: update footer and user
    
    * chore: add .DS_Store to .gitignore
    
    * feat: make all buttons functional
    
    * feat:add header to the top
    
    * feat: update header animation to flip text
    
    * fix: fix background for dropdown menu
    
    * feat: update button text
    
    * feat: finish about page
    
    * fix: fix navBar position
    
    * feat: update button text (#1)
    
    * feat: update button text
    
    * feat: finish about page
    
    * fix: fix navBar position
    
    * feat: add meet the team cards
    
    * feat: add contact us page
    
    * feat: finish login signup page
    
    * fix: fix typo in login and signup page
    
    * fix: fix login and signup button typo
    
    * fix: fix merge conflict
    
    * fix: fix login and signup button typo
    
    * fix: fix all merge conflict
    
    * feat: complete rental guide page
    
    * feat: add sections in navbar
    
    * feat: add filter section in home page
    
    * feat: finish home page
    
    * feat: complete prepare document page
    
    * feat: update logo
    
    * feat: add functioning filter bar in home page
    
    * feat: add filters in findAHome page
    
    * feat: complete Coverletter Accordion in prepare doc page
    
    * feat: complete ParentLetter Accordion in prepare doc page
    
    * feat: finish rental guide page
    
    * fix: remove i18n that is not supported
    
    * style: improve navBar and search UI layout
    
    * style: make guide page ui prettier
    
    * style: update guide card from 3 columns to 4 columns
    
    * feat: add home just for you card
    
    * style: make page filter prettier
    
    * feat: make website also support chinese
    
    * feat: connect login and register api
    
    * fix: fix url writing
    
    * fix: fix merge conflict

--- packages/frontend/src/app/[locale]/signup/page.tsx
+++ packages/frontend/src/app/[locale]/signup/page.tsx
@@ -28,13 +28,14 @@ const Signup = () => {
       const baseurl = await getApiBaseUrl();
       console.log(baseurl);
 
-      const res = await fetch(`${baseurl}/auth/register`, {
+      const res = await fetch(`http://${baseurl}/auth/register`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ email, password }),
       });
+
       if (!res.ok) {
         throw new Error('Signup failed');
       }
