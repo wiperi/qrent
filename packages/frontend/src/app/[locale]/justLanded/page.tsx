f2543ed6fa30a8d4b93f56a2d64fbcab58038b6c
Ting Bai <baiyuting652@gmail.com>
2025-04-02 16:39:19 +1100

    config: enable CORS (#30)
    * feat: add logo and login in landing router
    
    * style: update font for logo and login button
    
    * feat: add button component and update login NavLink to button
    
    * style: update color theme to Morandi
    
    * feat: add menu list animation
    
    * style: adjust spacing between list items for better usability
    
    * add new slideshow
    
    * feat: add language button
    
    * fix: change slideshow to fixed position on page
    
    * fix: correct typo
    
    * fix: resolve excessive height space issue
    
    * feat: add footer
    
    * feat: update footer and user
    
    * chore: add .DS_Store to .gitignore
    
    * feat: make all buttons functional
    
    * feat:add header to the top
    
    * feat: update header animation to flip text
    
    * fix: fix background for dropdown menu
    
    * feat: update button text
    
    * feat: finish about page
    
    * fix: fix navBar position
    
    * feat: update button text (#1)
    
    * feat: update button text
    
    * feat: finish about page
    
    * fix: fix navBar position
    
    * feat: add meet the team cards
    
    * feat: add contact us page
    
    * feat: finish login signup page
    
    * fix: fix typo in login and signup page
    
    * fix: fix login and signup button typo
    
    * fix: fix merge conflict
    
    * fix: fix login and signup button typo
    
    * fix: fix all merge conflict
    
    * feat: complete rental guide page
    
    * feat: add sections in navbar
    
    * feat: add filter section in home page
    
    * feat: finish home page
    
    * feat: complete prepare document page
    
    * feat: update logo
    
    * feat: add functioning filter bar in home page
    
    * feat: add filters in findAHome page
    
    * feat: complete Coverletter Accordion in prepare doc page
    
    * feat: complete ParentLetter Accordion in prepare doc page
    
    * feat: finish rental guide page
    
    * fix: remove i18n that is not supported
    
    * style: improve navBar and search UI layout
    
    * style: make guide page ui prettier
    
    * style: update guide card from 3 columns to 4 columns
    
    * feat: add home just for you card
    
    * style: make page filter prettier
    
    * feat: make website also support chinese
    
    * feat: connect login and register api
    
    * fix: fix url writing
    
    * fix: fix merge conflict
    
    * fix: add rewrites to fix mixed content error
    
    * chore: change session store to local store
    
    * fix: fix dailyhouse page fetch url
    
    * feat: connect to house filter api in efficiency filter page
    
    * feat: add area in filters
    
    * config: enable CORS

--- packages/frontend/src/app/[locale]/justLanded/page.tsx
+++ packages/frontend/src/app/[locale]/justLanded/page.tsx
@@ -1,19 +1,16 @@
 'use client';
 import HousingFilter from '@/src/components/HousingFilter';
 import HousingList from '@/src/components/HousingList';
-import { useFilterStore } from '@/src/store/useFilterStore';
 import React from 'react';
 
 const Page = () => {
-  const { filter, updateFilter } = useFilterStore();
-
   return (
     <div className="min-h-auto">
       {/* Main Content Layout */}
       <div className="flex flex-col lg:flex-row">
         {/* Left: Filter Sidebar */}
         <section className="flex-[2] bg-gray-50 rounded-lg p-5 shadow-md h-auto overflow-y-auto">
-          <HousingFilter filter={filter} setFilter={updateFilter} />
+          <HousingFilter />
         </section>
 
         {/* Right: Housing Listings */}
