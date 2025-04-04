7ffa68bad8c9814fbae0e3d57feaea2da8b44117
Yibin <z5567083@ad.unsw.edu.au>
2025-04-04 11:36:03 +1100

    fix: delete unused file

--- packages/shared/prisma/migrations/20250326012747_add_description_text/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `publishedAt` on the `Property` table. The data in that column could be lost. The data in that column will be cast from `DateTime(0)` to `DateTime`.
-
-*/
--- AlterTable
-ALTER TABLE `Property` ADD COLUMN `description` TEXT NULL,
-    MODIFY `publishedAt` DATETIME NULL;
