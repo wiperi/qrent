84d6d1f96efc0af4a17c4cf82e28a2382ac3fdce
fisher-byte <yyzyfish5@gmail.com>
2025-04-18 19:09:08 +1000

    feat: connect Clarity

--- packages/frontend/src/app/[locale]/layout.tsx
+++ packages/frontend/src/app/[locale]/layout.tsx
@@ -34,6 +34,19 @@ export default async function LocaleLayout({
             gtag('config', 'G-LVXN1Q8W0X');
           `}
         </Script>
+        <Script 
+          id="clarity-analytics" 
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "r5zysdcmja");
+            `,
+          }}
+        />
       </head>
       <body>
         <NextIntlClientProvider>
