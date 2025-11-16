7221f65f6d4937f29a5978e7ba03231edb663481
Jinzhu Zhao <jinzhu.zhao@student.unsw.edu.au>
2025-11-17 00:58:37 +1100

    feat: add filter tags display logic

--- packages/frontend/src/app/[locale]/search/page.tsx
+++ packages/frontend/src/app/[locale]/search/page.tsx
@@ -1,6 +1,8 @@
 import FilterModal from '@/components/FilterModal';
 import SearchBar from '@/components/SearchBar';
 import SearchResults from './SearchResults';
+// 横向: 添加
+import CurrentFiltersBar from '@/components/FilterTags';
 
 type SearchParams = {
   q?: string
@@ -9,6 +11,17 @@ type SearchParams = {
 
 export default async function SearchPage({ searchParams }: { searchParams: Promise<SearchParams> }) {
   const params = await searchParams;
+    //横向: 添加
+    const hasFilter = [
+    'propertyType',
+    'priceMin', 'priceMax',
+    'bedroomsMin', 'bedroomsMax',
+    'bathroomsMin', 'bathroomsMax',
+    'commuteMin', 'commuteMax',
+    'rating',
+    'moveInDate',
+    'areas'
+  ].some(key => params[key as keyof typeof params]);
 
   return (
     <main>
@@ -16,6 +29,13 @@ export default async function SearchPage({ searchParams }: { searchParams: Promi
       <section className="py-4">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <SearchBar />
+          
+          {/*横向*/}
+          {hasFilter && (
+            <div className="mt-4 rounded-xl border border-slate-200 bg-white px-4 py-3 shadow">
+              <CurrentFiltersBar />
+            </div>
+          )}
         </div>
       </section>
 
